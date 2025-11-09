532ef14e33d6bf9bd2b4c2038ca19dae949dfe6b
PapaMatze <mathiasgentz@freenet.de>
2025-11-09 13:39:10 +0100

    Add files via upload

---
+++ REAwheels.lua
@@ -0,0 +1,6 @@
+-- REA Wheels – Realismus Patch 1.0.5
+REAwheels = {}
+REAwheels.WheelSinkScale = 2.5
+REAwheels.RollingResistanceScale = 2.8
+REAwheels.WeightEffectScale = 2.6
+REAwheels.HelperEffect = true
